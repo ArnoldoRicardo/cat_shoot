2319b6d5f394f7090783a1f8530d7fcb3b04b1ce
ArnoldoRicardo <anyback@gmail.com>
2021-01-19 21:53:51 -0600

    configuracion de webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Empezamos yeih!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function Index() {
+  return <div>hola</div>;
+}
+
+ReactDOM.render(<Index />, document.getElementById('app'));
