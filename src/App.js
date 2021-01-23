72504357fde93b3f837ccbfc832ef46c8da62ec2
ArnoldoRicardo <anyback@gmail.com>
2021-01-22 19:19:26 -0600

    extructura visual de lista de categorias

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Category } from './components/Category';
+import { ListOfCategories } from './components/ListOfCategories';
+
+import { GlobalStyle } from './GlobalStyles';
 
 export const App = () => (
   <div>
-    <Category />
+    <GlobalStyle />
+    <ListOfCategories />
   </div>
 );
