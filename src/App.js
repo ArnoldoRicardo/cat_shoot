df5ef84361e6edde13c41531ab7971eae3d17e90
ArnoldoRicardo <anyback@gmail.com>
2021-01-23 19:11:25 -0600

    logo

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { GlobalStyle } from './GlobalStyles';
 
 import { ListOfCategories } from './components/ListOfCategories';
 import { ListOfPhotoCards } from './components/ListOfPhotoCards.js';
+import { Logo } from './components/Logo';
 
 export const App = () => (
   <div>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </div>
---
+++ src/components/Logo/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Svg = styled.svg`
+  width: 220px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: -30px;
+  margin-bottom: -30px;
+  display: block;
+`;
