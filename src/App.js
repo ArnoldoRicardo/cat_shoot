49a13bede8bc591a5c8e4fa90b7f4257693d59d9
ArnoldoRicardo <anyback@gmail.com>
2021-01-25 20:12:34 -0600

    animacion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { GlobalStyle } from './GlobalStyles';
+import { GlobalStyle } from './styles/GlobalStyles';
 
 import { ListOfCategories } from './components/ListOfCategories';
 import { ListOfPhotoCards } from './components/ListOfPhotoCards.js';
