33e32a1211952f953e414ea014fbf7fd27589b1d
ArnoldoRicardo <anyback@gmail.com>
2021-01-25 20:30:52 -0600

    como vamos a recibir los datos desde la api

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Category } from '../Category';
 
 import { List, Item } from './styles';
 
-import { categories } from '../../../api/db.json';
+import { categories as mockCategories } from '../../../api/db.json';
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    console.log('obtener datos de');
+    setCategories(mockCategories);
+  }, []);
+
   return (
     <List>
       {categories.map((category) => (
