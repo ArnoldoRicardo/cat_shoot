bc4a34434aba08eef12ecbcd75f57ee8d65eb29e
ArnoldoRicardo <anyback@gmail.com>
2021-02-01 14:39:03 -0600

    json server

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import { Category } from '../Category';
 
 import { List, Item } from './styles';
 
-import { categories as mockCategories } from '../../../api/db.json';
+// import { categories as mockCategories } from '../../../api/db.json';
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    console.log('obtener datos de');
-    setCategories(mockCategories);
+    const getData = async () => {
+      const response = await axios.get('http://localhost:8081/categories');
+      setCategories(response.data);
+    };
+    getData();
   }, []);
 
   return (
--- src/components/ListOfPhotoCards.js/index.js
+++ src/components/ListOfPhotoCards.js/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { PhotoCard } from '../PhotoCard';
 
-import { photos } from '../../../api/db.json';
+// import { photos } from '../../../db.json';
 
 export const ListOfPhotoCards = () => {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get('http://localhost:8081/photos');
+      setPhotos(response.data);
+    };
+    getData();
+  }, []);
+
   return (
     <ul>
       {photos.map((photoCard) => (
